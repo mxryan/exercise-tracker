73f3b785a7719af71e2d60755826735489c4866a
M Ryan <mxryan@live.com>
2018-12-10 20:04:56 -0500

    added logic for saving new exercise to db

--- server.js
+++ server.js
@@ -19,50 +19,53 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api/exercise/new-user", (req, res) => {
-  db.User.findOne({
-    username: req.body.username
-  }, (err, result) => {
-    if (err) {
-      res.status(500).send({
-        error: err
-      });
-      return console.log(err);
+  db.User.findOne({ username: req.body.username }, (findErr, foundUser) => {
+    if (findErr) {
+      res.status(500).send({ error: findErr });
+      return console.log(findErr);
     }
-    if (result) {
-      res.status(409).send({
-        message: "Username already exists"
-      });
-      console.log(result);
+    console.log("Found result: ", foundUser);
+    if (foundUser) {
+      res.status(409).send({ message: "Username already exists" });
+      console.log(foundUser);
     } else {
-      const user = new db.User({
-        username: req.body.username
-      });
-      console.log(user);
+      const user = new db.User({ username: req.body.username });
       user.save((saveErr, savedUser) => {
         if (saveErr) {
-          res.status(500).send({
-            error: saveErr
-          })
+          res.status(500).send({ error: saveErr });
           return console.log(saveErr);
         }
-        res.status(201).send({
-          data: savedUser
-        })
+        res.status(201).send({ data: savedUser });
       });
     }
   })
-
-
-
 });
 
 app.post("/api/exercise/add", (req, res) => {
   console.log(req.body);
-  const msg = {
-    msg: "You hit '/api/exercise/add' and this is what you sent me",
-    ...req.body
-  }
-  res.json(msg);
+  db.User.findOne({_id: req.body.userId}, (err, foundUser) => {
+    if (err) {
+      res.status(500).send({ error: err });
+      return console.log(err);
+    }
+    if (foundUser) {
+      const exercise = new db.Exercise({
+        userId: req.body.userId,
+        description: req.body.exerciseDescription,
+        duration: req.body.exerciseDuration,
+        date: req.body.exerciseDate
+      });
+      exercise.save((saveErr, savedExercise) => {
+        if (saveErr) {
+          res.status(500).send({error: saveErr});
+          return console.log(saveErr);
+        }
+        res.status(201).send({ data: savedExercise})
+      });
+    } else {
+      res.status(404).send({ message: "That user ID does not exist."});
+    }
+  })
 });
 
 app.listen(PORT, () => console.log("Server is listening"));
